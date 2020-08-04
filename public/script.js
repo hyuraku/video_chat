103ecd1f5f5fcd8a16fad675a25be8e4df9931ab
hyuraku <32809703+hyuraku@users.noreply.github.com>
2020-08-04 21:46:13 +0900

    play one video

--- public/script.js
+++ public/script.js
@@ -1,7 +1,32 @@
 const socket = io('/')
+const videoGrid = document.getElementById('video-grid')
+const myPeer = new Peer(undefined,{
+  host: '/',
+  port: 5001
+})
 
-socket.emit('join-room', ROOM_ID, 10)
+const myVideo = document.createElement('video')
+myVideo.muted = true
+
+navigator.mediaDevices.getUserMedia({
+  video: true,
+  audio: true
+}).then(stream => {
+  addVideoStream(myVideo, stream)
+})
+
+myPeer.on('open', id =>{
+  socket.emit('join-room', ROOM_ID, id)
+})
 
 socket.on('user-connected', userId =>{
   console.log(`user_id: ${userId}`)
 })
+
+function addVideoStream(video, stream){
+  video.srcObject = stream
+  video.addEventListener('loadedmetadata',()=>{
+    video.play()
+  })
+  videoGrid.append(video)
+}
